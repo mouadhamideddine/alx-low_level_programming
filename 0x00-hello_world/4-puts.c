a79bb49222fdd7811d97bbd861c7a1c38b9ea4b5
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-13 16:52:10 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry point for the program.
+ *
+ *description : print message
+ *
+ *Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
