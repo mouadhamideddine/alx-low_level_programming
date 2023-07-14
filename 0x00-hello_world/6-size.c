dde530003f22ba0c23612e0cbd9e97f8569b1f10
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-13 18:34:32 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,12 @@
  *
  *Return: Always 0 (Success).
  */
-
 int main(void)
 {
-printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-printf("Size of an int: %ld bytes(s)\n", sizeof(int));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %lu bytes(s)", sizeof(float));
 return (0);
 }
