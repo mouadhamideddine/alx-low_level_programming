691ff91b5388f45dbb91c3099b165920118d7af6
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-13 18:08:00 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point for the program.
+ *
+ *description : print message
+ *
+ *Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of a an int: %ld bytes(s)\n", sizeof(int));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+return (0);
+}
