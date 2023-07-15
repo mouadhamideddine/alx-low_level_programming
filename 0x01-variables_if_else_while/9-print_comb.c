6d074902856010baf55cfd2792e420b77ed38405
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-14 19:54:48 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * description : prints 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
