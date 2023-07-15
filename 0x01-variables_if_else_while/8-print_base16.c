618027bb46c64e30248cb73792edf48b074ea178
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-14 19:32:08 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry 
+ *
+ * description : prints hexa
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	count = 0;
+	for (; count <= 9; count++)
+	{
+		putchar('0' + count);
+	}
+	count = 0;
+	for (; count <= 5 ; count ++)
+	{
+		putchar('a' + count);
+	}
+	putchar('\n');
+	return (0);
+}
