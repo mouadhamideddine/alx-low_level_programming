fdd5c7af2cc2c80c424e8e4e08a6d7aa0103a2f7
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-14 18:25:39 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *description : print from 1 to 10
+ *
+ *Return : Always 0 (Success).
+ */
+main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
