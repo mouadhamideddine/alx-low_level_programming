55310a9e7f6ba27742aab40e814aab7f8944d1e0
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 16:29:16 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
-/** print_alphabet_x10 - function
+/**
+ *print_alphabet_x10 - function
  *
- * description : print alphabets 10 times
+ *description : print alphabets 10 times
  *
- * Return: Always none (success)
+ *Return: Always none (success)
  */
 void print_alphabet_x10(void)
 {
-	int Index ;
+	int Index;
 	int Count = 0;
-	
+
 	for (; Count < 10; Count++)
 	{
 		for (Index = 97; Index < 123; Index++)
