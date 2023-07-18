431a0107860810edff56157a654ae590dd09a591
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 16:18:00 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int Index = 97;
+	int Count = 0;
+	
+	for (; Count < 10; count++)
+	{
+		for (; Index < 123; Index++)
+		{
+			_putchar(Index);
+		}
+		_putchar('\n');
+	}
