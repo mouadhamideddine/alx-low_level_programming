c502a6b4ad97271e82af80a52752b40913323fc1
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 14:42:23 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry
+ *
+ * description : print _putchar
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
