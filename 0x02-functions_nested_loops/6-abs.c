85d4360a52fa4a082e9a9d26a1ab7d08d1b3d06c
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 18:02:21 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _abs - function
+ *
+ * description : return absolute of a number
+ *
+ * Return: Absolute of an integer
+ *
+ */
+int _abs(int x)
+{
+	if (x > 0)
+	{
+		return (x);
+	}
+	else if (x < 0)
+	{
+		return (-x);
+	}
+	else if (x == 0)
+	{
+		return (x);
+	}
+return (0);
+}
