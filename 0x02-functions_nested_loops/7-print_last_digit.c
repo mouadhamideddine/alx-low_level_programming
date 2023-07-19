04851df401d4cf9d31df1211f6efa5e08dcb5b0b
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 18:37:16 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - function
+ *
+ *description : print_last_digit
+ *
+ * Return: returns values of the last digit
+ */
+int print_last_digit(int x)
+{
+	int digit;
+	digit = x % 10;
+	_putchar(digit);
+	return (digit);
+}
