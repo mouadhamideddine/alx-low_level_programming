58315e2ea50e8091f45fa0edbffa8faa32215a68
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-19 07:28:06 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,29 +11,33 @@ void times_table(void)
 	int result;
 	int first_digit;
 	int second_digit;
+	int multiplier;
+	int table;
 
-	for (second = 0; second < 10; second++)
+	for (table = 0; table < 10; table++)
 	{
-		result = 9*second;
-		if (result > 9)
+		for (multiplier = 0; multiplier < 10; multiplier++)
 		{
-			first_digit = result / 10;
-			second_digit = result % 10;
-			_putchar(first_digit + '0');
-			_putchar(second_digit + '0');
-		}
-		else if (result < 10)
-		{
-		_putchar(result + '0');
-		}
-		if (result == 81)
-		{
-			break;
-		}
-		else
-		{
-		_putchar(',');
-		_putchar(' ');
-		}
-	}
+			result = table * multiplier;
+			if (result < 10)
+			{
+			_putchar(result +'0');
+			}
+			else if (result > 9)
+			{
+				first_digit = result / 10;
+				second_digit = result % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+			}
+			if (multiplier ==9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
 }
