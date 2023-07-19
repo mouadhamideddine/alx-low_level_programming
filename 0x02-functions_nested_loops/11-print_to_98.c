f34ec7961ea4fd0f72c48d6ecb9a00f95e0d9854
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-19 12:01:04 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ *
+ *description : prints from n to 98
+ *
+ * @n : input number
+ *
+ *Return : Always None (success)
+ */
+void print_to_98(int n)
+{
+	int first_digit;
+	int second_digit;
+	int third_digit;
+
+	if (n>98)
+	{
+		for (; n => 98; n--)
+		{
+			if (n == 98)
+			{
+				first_digit = n / 10;
+				second_digit = n % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+				_putchar('\n');
+				break;
+			}
+			if (n>99)
+			{
+				first_digit = n / 100;
+				second_digit = ((n / 10) % 10);
+				third_digit = n % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+				_putchar(third_digit + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (n < 99)
+			{
+				first_digit = n / 10;
+				second_digit = n % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+			}
+		}
+	}
+	if (n<98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n ==98)
+			{
+				first_digit = n / 10;
+				second_digit = n % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+				_putchar('\n');
+				break;
+			}
+			else if (n < 98)
+			{
+				first_digit = n / 10;
+				second_digit = n % 10;
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
