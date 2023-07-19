941bd967fb50109879addb81e2e04440c0a79845
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-18 17:48:43 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  *
  * @n: number
  *
- * Return: Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
+ * Return: Returns 1 if n > 0, Returns 0 if n = 0, Returns -1 if < 0
  */
 int print_sign(int n)
 {
