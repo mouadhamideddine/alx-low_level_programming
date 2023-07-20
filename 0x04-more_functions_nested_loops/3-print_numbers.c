48b90186b60d3eaa6e156f181d9dc9fa76ac2a5c
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 13:25:09 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - from 0 to 9
+ * description : look up
+ * Return: None
+ */
+
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		_putchar(count + '0');
+	}
+}
