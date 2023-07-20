7acb9a2db734a653de82abfe096d57b19340c2e5
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 13:36:32 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - except 2 and 4
+ * description : look above
+ * Return: None
+ */
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		if (count == 2 || count == 4)
+		{
+			;
+		}
+		else
+		{
+			_putchar(count + '0');
+		}
+	}
+}
