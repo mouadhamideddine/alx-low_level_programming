5527c187263458961f0a124519c3377fe82a0d73
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 08:44:01 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_isupper - checks if upper
+ * @c : inputto check
+ * Return: 1 if c uppercase 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c =< 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
