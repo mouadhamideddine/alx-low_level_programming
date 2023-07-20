a3ccde48d0a9b2e7733f7748ed9a1ce09db16f4d
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 16:39:34 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print lines _
+ * description : above
+ * @n : number of times to write _
+ * Return: None 
+ */
+void print_line(int n)
+{
+	for (count = 0; count < n; count++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
