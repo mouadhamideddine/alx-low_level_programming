f6629ef465ef13f3816a262af9d08cbb03154d0b
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-19 17:51:44 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -25,6 +25,6 @@ int largest_number(int a, int b, int c)
     {
         largest = c;
     }
-    printf("%d is the largest number", largest) 
+    printf("%d is the largest number", largest);
     return (largest);
 }
