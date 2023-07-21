a02aef0af4bf19077caca3c39aa4ab1228256115
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 19:58:01 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,24 @@
  */
 void print_triangle(int size)
 {
-        int row, col;
-
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (row = 1; row <= size; row++)
-                {
-                        for (col = 1; col <= size - row; col++)
-                        {
-                                _putchar(' ');
-                        }
-                        for (col = 1; col <= row; col++)
-                        {
-                                _putchar('#');
-                        }
-                        _putchar('\n');
-                }
-        }
+	int row, col;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= size - row; col++)
+			{
+				_putchar(' ');
+			}
+			for (col = 1; col <= row; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
