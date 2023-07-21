e3f31a26080dc3eaab62a27703d76e6b21cc4d46
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-20 19:09:06 -0700

    first try

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**main - entry
+ * description : print 1 to 100 _ for %3= 0 Fizz _for %5=0 buzz _for %5=0 and %3=0 fizzbuzz
+ * Return: None
+ */
+void main(void)
+{
+	int count;
+
+	for (count = 1; count < 101; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", count);
+		}
+	}
+	putchar('\n');
+}
