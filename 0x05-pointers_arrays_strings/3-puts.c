ef7710c89fa6bf80204e0826118df4a1bf0d1cd7
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-24 17:03:41 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - funciton
+ * description : prints a string followed by a new line
+ * @str : string pointer to be printed
+ * Return: None
+ */
+void _puts(char *str)
+{
+	char *protect = str;
+
+	while (*protect != '\0')
+	{
+		_putchar(*protect);
+		*protect++;
+	}
+	_putchar('\n');
+}
