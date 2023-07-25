c80bf5538d25f0ebadd808b9c178fca7203d07ba
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-24 19:15:23 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
-void rev_string(char* s)
-{
-	if (s == nullptr)
-	{
-		return;
-	}
-	size_t len = 0;
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
 
-	while (s[len] != '\0')
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		++len;
-	}
-	for (size_t i = 0; i < len / 2; ++i)
-	{
-		char tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
