2a78a70a681f25c83298315ea7e0fd709e992e30
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-24 18:02:04 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * description : look above
+ * @s : string
+ * Return: None
+ */
+void print_rev(char *s)
+{
+	/**
+	 * determine length of str*/
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	/** iterate through the word in reverse */
+	for (; length >=0 ; length--)
+	{
+		_putchar(*s);
+		s--;
+	}
+}
+
