66398c1b2f1e9d308745b412450373225d5110c0
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-25 19:23:02 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * description :  print the second half of the string
+ * @str : string
+ * Return: None
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int to_be; /* num of chars to be printed */
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		to_be = length / 2;
+	}
+	else
+	{
+		to_be = (length -1) / 2;
+	}
+	for (;to_be < length; to_be++)
+	{
+		_putchar(str[to_be]);
+	}
+	_putchar('\n');
+}
