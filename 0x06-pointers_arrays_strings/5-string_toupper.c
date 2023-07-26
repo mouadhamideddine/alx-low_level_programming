78347f4ad9d366161ded753aab31fd6f504ddb5e
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 16:58:47 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to upper
+ * Return: Pointer
+ */
+char *string_toupper(char *str)
+{
+	int length_str = 0;
+	int asci_value = 0;
+	char ch;
+
+	while (str[length_str] != '\n')
+	{
+		length_str++;
+	}
+	for (; length_str > 0; length_str--)
+	{
+		asci_value = (int) str[length_str];
+		if (asci_value >= 65 && asci_value <= 90)
+		{
+			;
+		}
+		else if (asci_value >= 97 && asci_value <= 122)
+		{
+			asci_value = asci_value - 32;
+			ch = (char) asci_value;
+			str[length_str] = ch;
+		}
+	}
+	return (str);
+}
