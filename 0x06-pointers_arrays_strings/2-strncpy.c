e2dc3b620d23f3bfc8a1f5cd3489131c0431452b
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 12:37:15 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strncpy - copies a string.
+ *@dest : destination string
+ *@src : source string
+ *@n : maximum number of chars to be copied
+ *return: pointer to src
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (; n > 0;n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
