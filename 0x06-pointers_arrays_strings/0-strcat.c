b022821d5c0c520773a71dfe7f0efbcd74f96736
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 09:09:28 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,14 +3,14 @@
  * _strcat - appends a string to the eng of a string
  * @dest : destination string
  * @src : source string
- * Return : pointer to the string
+ * Return: pointer to the string
  */
 char *_strcat(char *dest, char *src)
 {
 	int length_dest = 0;
 
 	/**determine size of dest*/
-	while(dest[length_dest] != '\0')
+	while (dest[length_dest] != '\0')
 	{
 		length_dest++;
 	}
