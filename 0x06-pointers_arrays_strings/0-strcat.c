d34eb83ac9205636342085a35e06bdbc4b9a6d4f
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 09:07:42 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - appends a string to the eng of a string
+ * @dest : destination string
+ * @src : source string
+ * Return : pointer to the string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length_dest = 0;
+
+	/**determine size of dest*/
+	while(dest[length_dest] != '\0')
+	{
+		length_dest++;
+	}
+	/**determine size of src including null* NO Need you just append it to dest*/
+	while (*src != '\0')
+	{
+		dest[length_dest] = *src;
+		length_dest++;
+		src++;
+	}
+	length_dest++;
+	dest[length_dest] = '\0';
+	return (dest);
+}
