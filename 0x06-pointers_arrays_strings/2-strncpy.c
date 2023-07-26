522f34820663e3fdddf8ca7315d5c31bd263e5de
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 13:35:36 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *start = dest;
-	for (; n > 0;n--)
+	size_t i;
+	for (i = 0; i < n && src[i] != '\0' ;i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src [i];
 	}
-	*dest = '\0';
-	return(start);
+	for (; i < n; i++)
+	{
+		dest[i] = src [i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
