f89df03ddd3169859c98597098fea62032f5299c
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-27 14:57:37 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,50 @@
 #include "main.h"
-
+/**
+ * cap_string - capitalize all first characters of words
+ * @str : string
+ * Return: Pointer
+ */
 char *cap_string(char *str)
 {
-	int token = 1;
+	char delimiters[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	char *p = str;
-	while (*p != '\0')
-	{
-	if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || *p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' || *p == '}')
-	{
-		token = 1;
-	}
-	else if (*p >= 'a' && *p <= 'z' && token == 1)
-	{
-		*p -= ('a' - 'A');
-		token = 0;
-	}
+	int token = 1;
+	int delimiter_incre;
 
-	p++;
+	while (*p != ('\0'))
+	{
+
+		/**Delimiter search*/
+		if (token == 0)
+		{
+			for (delimiter_incre = 0; delimiter_incre < 13; delimiter_incre++)
+			{
+				if (*p == delimiters[delimiter_incre])
+				{
+					token = 1;
+					p++;
+					break;
+				}
+			}
+			if (token == 0)
+			{
+				p++;
+			}
+		}
+		/**End*/
+
+		/** START code to replace a character to upper*/
+		if ((*p >= 'a' && *p <= 'z') && token == 1)
+		{
+			*p -= ( 'a' - 'A');
+			token = 0;
+			p++;
+		}
+		else
+		{
+			token = 0;
+		}
+		/**END*/
 	}
 	return (str);
 }
-
