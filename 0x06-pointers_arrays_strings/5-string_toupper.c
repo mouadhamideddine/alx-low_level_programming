bbac35eafdd9d9af44e48900198a68036c78fa21
mouadhamideddine <mouadhamideddine@gmail.com>
2023-07-26 17:25:18 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,35 +5,19 @@
  */
 char *string_toupper(char *str)
 {
-	int length_str = 0;
-	int asci_value = 0;
-	char ch;
+	char *p = str ;
 
 	if (*str == '\0')
 	{
 		return(str);
 	}
-	while (str[length_str] != '\n')
+	while (*p != ('\0'))
 	{
-		length_str++;
-	}
-	for (; length_str > 0; length_str--)
-	{
-		asci_value = (int) str[length_str];
-		if (asci_value >= 65 && asci_value <= 90)
+		if (*p >= 'a' && *p <= 'z')
 		{
-			;
-		}
-		else if (asci_value >= 97 && asci_value <= 122)
-		{
-			asci_value = asci_value - 32;
-			ch = (char) asci_value;
-			str[length_str] = ch;
-		}
-		else
-		{
-			;
+			*p -= ('a' - 'A');
+			p++;
 		}
 	}
-	return (str);
+	return(str);
 }
