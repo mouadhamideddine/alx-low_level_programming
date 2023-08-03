80b428fb13f730121ad7a403199aa49ebe4ac32e
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-03 14:06:06 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - fucntion
+ * @s : string provided
+ * @accept : accept set of chars
+ * Return: the number of bytes in the initial segment @s that consist only of @accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	char *p_accept = accept;
+
+	while (*s != '\0')
+	{
+		while (*p_accept != *s)
+		{
+			if (*s == '\0')
+			{
+				return (length);
+			}
+			p_accept++;
+		}
+		p_accept = accept;
+		length++;
+		s++;
+	}
+	return (length);
+}
