7a81c3222234f2d7ab7ba04b56c91328fbea2751
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-09 19:36:47 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,11 @@
 /**
  * main - prints name of the program
  * @argv : argv
+ * @argc : argument count
  * Return: None
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-	return(0);
+	return (0);
 }
