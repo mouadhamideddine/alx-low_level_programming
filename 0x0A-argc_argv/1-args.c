db61f26d4006a9ef2cf2207c2a3fe0bf3b9f4816
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-09 19:43:05 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print number arg passed
+ * @argc : number of argument
+ * @argv : vector argument
+ * Return: 0 (success)
+ */
+int main (argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+}
