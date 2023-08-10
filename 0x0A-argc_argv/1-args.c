efbdbe216733c7a36344e20309da336c09492ff8
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-09 19:48:06 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv : vector argument
  * Return: 0 (success)
  */
-int main (int argc, char *argv[])
+int main (int argc, char *argv[]) __attribute__((unused))
 {
 	printf("%d\n", argc);
 	return (0);
