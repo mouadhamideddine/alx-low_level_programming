7dc3547c3c4a129aa331f0828c5d0d56f62132ef
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-09 19:35:00 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv : argv
  * Return: None
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return(0);
