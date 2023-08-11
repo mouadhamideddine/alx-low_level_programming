0cc8f9021b3badd1ef94f4cd1fb9e7baa3fe4415
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-10 18:26:01 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	result = malloc(nmemb * size);
 	if (result == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
 	for (count = 0; count < nmemb; count++)
 	{
