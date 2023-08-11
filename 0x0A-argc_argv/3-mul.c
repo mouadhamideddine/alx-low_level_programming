26a28eb97100b086d0bfcad26c88da5523a836bf
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-11 09:56:40 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,16 +6,16 @@
  * @argv : array of arguments vector
  * Return: 0 prints multiplication , fail prints error/n and 1
  */
-int main (int argc, char  *argv[])
+int main(int argc, char  *argv[])
 {
 	int result;
 
-	if( argc < 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
-	return(0);
+	return (0);
 }
