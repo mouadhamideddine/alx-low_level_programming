b29673f046e1abcb00170ce5fb70ee442a8b1cdd
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-22 16:39:39 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,19 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * string_length - Calculates the length of a string.
- * @str: The input string.
- *
- * Return: The length of the string.
- */
-/**
  * _strncpy - Copies a string up to n bytes.
  * @dest: The destination string.
  * @src: The source string.
  * @n: The maximum number of bytes to copy.
  *
  * Description: This function copies at most n bytes from the string pointed to
- * by src to the buffer pointed to by dest. If the length of src is less than n,
+ * by src to the buffer pointed to by dest. If the length of src is > than n.
  * the remaining bytes in dest are filled with null bytes.
  */
 void _strncpy(char *dest, const char *src, size_t n)
@@ -31,14 +25,21 @@ void _strncpy(char *dest, const char *src, size_t n)
 		dest[i] = '\0';
 	}
 }
+/**
+ * string_length - Calculates the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
 int string_length(const char *str)
 {
-    int length = 0;
+	int length = 0;
 
-    while (str[length] != '\0') {
-	    length++;
-    }
-    return length;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
 /**
  * new_dog - creates a new dog
@@ -63,14 +64,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	_strncpy(new_dawg->owner, owner,(length + 1));
+	_strncpy(new_dawg->owner, owner, (length + 1));
 	new_dawg->age = age;
 	length = string_length(name);
-	new_dawg->name = malloc ((sizeof(char) * (length + 1)));
+	new_dawg->name = malloc((sizeof(char) * (length + 1)));
 	if (new_dawg->name == NULL)
-		{
-			return(NULL);
-		}
-	_strncpy(new_dawg->name, name,(length + 1));
-	return(new_dawg);
+	{
+		return (NULL);
+	}
+	_strncpy(new_dawg->name, name, (length + 1));
+	return (new_dawg);
 }
