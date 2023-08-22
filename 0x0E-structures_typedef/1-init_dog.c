c949b050ccc07010181be063041020b09739619c
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-22 11:03:11 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * init_dog - creates a struct
+ * @dog : char parametre
+ * @name : char parametre
+ * @age : float parametre
+ * @owner : char parametre
+ * Return: None
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
