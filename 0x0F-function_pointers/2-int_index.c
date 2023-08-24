df5afd32ebdacf083bd8c214f7382de7f530f9de
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-23 18:35:41 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns the index of the first element for
+ * which the cmp function does not return 0
+ * @array: of ints
+ * @size : of the array
+ * @cmp : function pointer
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count = 0;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return(-1);
+	}
+	for (; count < size; count ++)
+	{
+		if (cmp(array[count]) != 0)
+		{
+			return(count);
+		}
+	}
+}
