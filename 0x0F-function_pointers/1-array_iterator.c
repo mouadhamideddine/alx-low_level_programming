af946fce397893308abebdef831b90e0f892d860
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-23 18:02:23 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints each element in @array
+ * @array : array of ints
+ * @size : of the array
+ * @action : pointer to a func
+ * Return: None
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size--;
+	int count = 0;
+	for (; count < size; count++)
+	{
+		action(array[count]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
+int _putchar(int c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
