651a14840b14805b220c30320c693fede652752a
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-25 13:52:15 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -47,7 +47,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		current->str = strdup(str);
 		current->len = _strlen(str);
 		current->next = NULL;
-		*head = current;
+		(*head)->next = current;
 		return (current);
 	}
 }
