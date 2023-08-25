fa937b2981c190b6fe88e071ed5ca88f6f6d06be
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-25 13:29:41 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
 #include "main.h"
+/**
+ * _strlen : calculate length
+ * @str : string
+ * return: size of str
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
 /**
  * add_node_end - adds a new node to the end
  * @head : head of the node
@@ -13,4 +29,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	if (head == NULL)
+	if (*head == NULL)
+	{
+		current->str = strdup(str);
+		current->len = _strlen(str);
+		current->next = NULL;
+		*head = current;
+		return (*head);
+	}
+	else
+	{
+		while((*head)->next != NULL)
+		{
+			*head = (*head)->next;
+		}
+		current->str = strdup(str);
+		current->len = _strlen(str);
+		current->next = NULL;
+		(*head)->next = current;
+		return (current);
+	}
+}
