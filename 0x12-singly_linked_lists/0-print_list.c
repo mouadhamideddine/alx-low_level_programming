4994be87b1b103a1296e875eb7045dc302f8df28
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-24 18:58:22 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_list - prints the nodes
+ * @h : pointer to a list_t structure (assuming the head)
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("[%d] %s\n",current->len, current->str);
+		count++;
+		current = current->next;
+	}
+	return(count);
+}
