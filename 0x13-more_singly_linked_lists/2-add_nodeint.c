c3ce8e25cdd4fd0b2439e81308945cc1edb55b49
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-28 11:02:02 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,13 +2,14 @@
 #include "lists.h"
 /**
  * add_nodeint - adds a node at the beginning
- * @h : pointer to a pointer to a listint_t struct represent head
+ * @head : pointer to a pointer to a listint_t struct represent head
  * @n : number
  * Return: Pointer to a listint_t struct (new head)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *current = malloc(sizeof(listint_t));
+
 	if (!current)
 	{
 		return (NULL);
