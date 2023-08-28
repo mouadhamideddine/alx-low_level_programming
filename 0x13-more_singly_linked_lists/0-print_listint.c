c7f8e1843fe48945e6424c153e6bc93ec1e80a3e
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-28 10:05:26 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints list
+ * @h : head of linked list
+ * Return: list size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+	{
+		printf("0");
+		return (0);
+	}
+	while (h->next != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
