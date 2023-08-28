ca9a15cceee5a40b4bdd13751d4a0863d2d50f62
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-28 16:27:43 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	if (!head)
 	{
@@ -19,6 +19,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	while(head)
 	{
+		if (index < i)
+		{
+			return (NULL);
+		}
 		if (index == i)
 		{
 			return (head);
