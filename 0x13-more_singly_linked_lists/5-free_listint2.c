bc20f3bf7c7963901283546a544210b9b2b01d3c
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-28 11:50:01 -0700

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees list sets head to null
+ * @head : head of the list
+ * Return: None
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *saver = *head;
+
+	if (!*head)
+	{
+		return;
+	}
+	while(*head)
+	{
+		saver = *head->next;
+		free(*head);
+		head = saver;
+	}
+	*head = NULL;
+}
