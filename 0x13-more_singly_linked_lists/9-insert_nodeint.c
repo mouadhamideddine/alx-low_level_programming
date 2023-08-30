7521fc1751c25a060871604530b8248780ecb28a
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-29 19:50:37 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,19 +13,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *current = *head;
 
-	if (!new_node)
+	if (!new_node || !(*head))
 	{
 		return (NULL);
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if (!(*head) || idx == 0)
+	if (idx == 0)
 	{
 		new_node->next = (*head)->next;
 		*head = new_node;
 		return(new_node);
 	}
-	while(current)
+	while(current && i < idx)
 	{
 		if (count == (idx - 1))
 		{
