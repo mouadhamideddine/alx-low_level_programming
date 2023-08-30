ca339f6b7359512fa17a688050db1b52ca839faf
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-29 19:57:57 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,13 +21,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 	if (idx == 0)
 	{
-		new_node->next = (*head)->next;
+		new_node->next = (*head);
 		*head = new_node;
 		return(new_node);
 	}
-	while(current && count < (idx - 1))
+	while(current && count < idx - 1)
 	{
-		if (count == (idx - 1))
+		if (count == idx - 1)
 		{
 			new_node->next = current->next;
 			current->next = new_node;
