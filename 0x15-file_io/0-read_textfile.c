3b6ef86e8b3bc8eb2e6aa88423b51bece2668c08
mouadhamideddine <mouadhamideddine@gmail.com>
2023-09-05 09:45:08 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,8 +25,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(char)*(letters));
+	buffer = malloc(sizeof(char)*(letters + 1));
 	Bytes_Read = read(file, buffer, letters);
+	buffer[Bytes_Read] = '\0';
 	Bytes_Written = write(STDOUT_FILENO, buffer, Bytes_Read);
 	free(buffer);
 	close(file);
