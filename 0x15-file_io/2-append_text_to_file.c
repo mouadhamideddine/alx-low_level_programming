352eeee39cfd9ea20b71aa683bffe1f032b23131
mouadhamideddine <mouadhamideddine@gmail.com>
2023-09-05 13:52:07 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content && fd == -1)
 		return (-1);
 
-	if(!text_content && fd != -1)
+	if (!text_content && fd != -1)
 		return (1);
 
 	bytes_w = write(fd, text_content, _strlen(text_content));
@@ -45,5 +45,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (bytes_w == -1)
 		return (-1);
 
-	return(1);
+	return (1);
 }
