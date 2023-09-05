15960b83697acc9b7ae56d319e2d7e2a50b712c3
mouadhamideddine <mouadhamideddine@gmail.com>
2023-09-05 09:13:19 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - read file
+ * @filename : file name
+ * @letters : how many letters to print
+ * Return: returns the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	ssize_t Bytes_Read, Bytes_Written;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(letters +1);
+	Bytes_Read = read(file, buffer, letters);
+	Bytes_Written = write(STDOUT_FILENO, buffer, Bytes_Read);
+	return (Bytes_Written);
+	free(buffer);
+	close(file);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+# include <sys/types.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
