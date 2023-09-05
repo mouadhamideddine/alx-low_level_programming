946d5ee5b56e2b692923cf87eaae4bd765b4fc83
mouadhamideddine <mouadhamideddine@gmail.com>
2023-09-05 13:10:41 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,6 +42,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (!text_content)
+	{
+		return (1);
+	}
 	if (text_content)
 	{
 		len = _strlen(text_content);
