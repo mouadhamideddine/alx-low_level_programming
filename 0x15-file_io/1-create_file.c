be2ba08760ed9a1cb08290ee904227b13022e8e7
mouadhamideddine <mouadhamideddine@gmail.com>
2023-09-05 11:32:49 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - Calculate the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string (excluding the null terminator).
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	return length;
+}
+/**
+ * create_file - creates file
+ * @filename :name of the file to be created
+ * @text_content = string/text
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t Bytes_w;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	Bytes_w = write (fd, text_content,_strlen(text_content));
+	if (Bytes_w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H
 # include <sys/types.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
