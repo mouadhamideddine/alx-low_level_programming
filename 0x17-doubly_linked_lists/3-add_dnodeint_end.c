6f9a887c19f9cd3c6ba22b4cadce6d3b794d9768
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-09 16:32:25 +0100

    betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -47,5 +47,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->prev = previous_node;
 	previous_node->next = new_node;
 	/*print_dlistint(previous_node);*/
-	return(new_node);
+	return (new_node);
 }
