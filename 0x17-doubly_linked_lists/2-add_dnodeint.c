3b3cadd73d1fe660cab54cd5af40450f3bc01f24
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-09 14:21:10 +0100

    0

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * add_dnodeint - adds a node at the beginning
  * @head : pointer to pointer to head node
@@ -9,17 +10,33 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	//printf("here\n");
 	if (!new_node)
 	{
+		free(new_node);
 		return (NULL);
 	}
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
-	(*head)->prev = new_node;
+	if(*head)
+	{
+		(*head)->prev = new_node;
+	}
 	*head = new_node;
-	return (new_node);
+	//new_node->next = malloc(sizeof(dlistint_t));
+	//printf("here\n");
+	//new_node->n = n;
+	//printf("new_node->n n=%d\n",new_node->n);
+	//new_node->next = *head;
+	//printf("new_node->next%p\n",new_node->next);
+	//new_node->prev = NULL;
+	//printf("new_node->prev");
+	//(*head)->prev = new_node;
+	//printf("(*head)->prev = new_node;");
+	//*head = new_node;
+	return (*head);
 }
