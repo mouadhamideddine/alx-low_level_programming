e6e87a160029e1fc022dcf4d519e729f0a5ba1c0
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-09 22:20:00 +0100

    typo

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t current = head;
+	dlistint_t *current = head;
 	unsigned int count = 0;
 
 	while (current)
