5196350646d5d9e6d667636c1330aa91cdb0eb98
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-13 15:31:07 +0100

    script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
+gcc -c -Wall -Werror -fpic *.c
+gcc -shared -o liball.so *.o
+rm *.o
 
-temp_dir=$(mktemp -d)
-
-cp *.c $temp_dir
-
-cd $temp_dir
-
-gcc -fPIC -shared -o liball.so *.c
-
-mv mylibrary.so ..
-
-cd ..
-rm -r $temp_dir
