d2d93ed38664b8099b9ee323049cc0713e90baf0
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-11-13 15:18:09 +0100

    dynamic library

---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,106 @@
+#include "main.h"
+#include <stddef.h>
+int _putchar(char c) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _islower(int c) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _isalpha(int c) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _abs(int n) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _isupper(int c) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _isdigit(int c) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+int _strlen(char *s) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+void _puts(char *s) {
+	    /* Replace this comment with your implementation */
+}
+
+char *_strcpy(char *dest, char *src) {
+	    /* Replace this comment with your implementation */
+	    return dest;
+}
+
+int _atoi(char *s) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+	    /* Replace this comment with your implementation */
+	    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+	    /* Replace this comment with your implementation */
+	    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+	    /* Replace this comment with your implementation */
+	    return dest;
+}
+
+int _strcmp(char *s1, char *s2) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+	    /* Replace this comment with your implementation */
+	    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+	    /* Replace this comment with your implementation */
+	    return dest;
+}
+
+char *_strchr(char *s, char c) {
+	    /* Replace this comment with your implementation */
+	    return NULL;
+}
+
+unsigned int _strspn(char *s, char *accept) {
+	    /* Replace this comment with your implementation */
+	    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+	    /* Replace this comment with your implementation */
+	    return NULL;
+}
+
+char *_strstr(char *haystack, char *needle) {
+	    /* Replace this comment with your implementation */
+	    return NULL;
+}
+/*
+void main(void)
+{
+	printf("nothing coded\n");
+}
+*/
