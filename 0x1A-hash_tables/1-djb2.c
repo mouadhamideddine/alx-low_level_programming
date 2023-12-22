b535ed85b7300093b9d2aa1baadd2bebd8f76043
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-12-22 22:51:01 +0100

    betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,9 @@
 #include "hash_tables.h"
+/**
+ * hash_djb2 - algorith
+ * @str: stirng
+ * Return: (hash value)
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,7 +2,7 @@
 /**
  * key_index - produces index where to store a key
  * @key: str to store
- * size: size of the hashtable array
+ * @size: size of the hashtable array
  * Return: unsigned long int: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
