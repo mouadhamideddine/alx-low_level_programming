7b8ba69b2668066ba2ad75238ce611597bc28427
Mouad Hamideddine <mouad.hamideddine@gmail.com>
2023-12-22 23:52:00 +0100

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash_table_t
+ * @ht: hash_table_t
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current = NULL;
+	long unsigned int index = 0;
+	int space_token = 1;
+
+	if (!ht)
+	{
+		return;
+	}
+	printf("{");
+	while (index < ht->size)
+	{
+		current = ht->array[index];
+		while(current)
+		{
+			if(space_token > 0)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+				space_token = -1;
+			}
+			else
+			{
+				printf(", '%s': '%s'", current->key, current->value);
+			}
+			current = current->next;
+		}
+		index++;
+	}
+	printf("}\n");
+	return;
+}
+			
--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -1,19 +1,65 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
+void unpack_hash_table(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *current = NULL;
+    if (ht == NULL || ht->array == NULL)
+    {
+        printf("Invalid hash table.\n");
+        return;
+    }
+
+    printf("Unpacking Hash Table:\n");
+    printf("Size: %lu\n", ht->size);
+
+    for (i = 0; i < ht->size; ++i)
+    {
+        if (ht->array[i] != NULL)
+        {
+            current = ht->array[i];
+
+            printf("\nBucket %lu:\n", i);
+
+            while (current != NULL)
+            {
+                printf("  Key: %s, Value: %s\n", current->key, current->value);
+                current = current->next;
+            }
+        }
+    }
+}
+
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_get - retrieves a value associated with a key in a hash table
+ * @ht: hash table
+ * @key: key to search for
+ * Return: value associated with the key, or NULL if key is not found
  */
+
+void unpackStructs(hash_node_t *head)
+{
+    hash_node_t *current = head;
+
+    while (current != NULL)
+    {
+        printf("Key: %s, Value: %s\n", current->key, current->value);
+        current = current->next;
+    }
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
 int main(void)
 {
     hash_table_t *ht;
-    char *value;
 
     ht = hash_table_create(1024);
+    hash_table_print(ht);
     hash_table_set(ht, "c", "fun");
     hash_table_set(ht, "python", "awesome");
     hash_table_set(ht, "Bob", "and Kris love asm");
@@ -21,23 +67,7 @@ int main(void)
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "c", "isfun");
-
-    value = hash_table_get(ht, "python");
-    printf("%s:%s\n", "python", value);
-    value = hash_table_get(ht, "Bob");
-    printf("%s:%s\n", "Bob", value);
-    value = hash_table_get(ht, "N");
-    printf("%s:%s\n", "N", value);
-    value = hash_table_get(ht, "Asterix");
-    printf("%s:%s\n", "Asterix", value);
-    value = hash_table_get(ht, "Betty");
-    printf("%s:%s\n", "Betty", value);
-    value = hash_table_get(ht, "98");
-    printf("%s:%s\n", "98", value);
-    value = hash_table_get(ht, "c");
-    printf("%s:%s\n", "c", value);
-    value = hash_table_get(ht, "javascript");
-    printf("%s:%s\n", "javascript", value);
+    hash_table_print(ht);
     return (EXIT_SUCCESS);
 }
+
